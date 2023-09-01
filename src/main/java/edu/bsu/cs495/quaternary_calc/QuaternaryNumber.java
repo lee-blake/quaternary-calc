3db6522a8aff853d9d3dd202cc34911b3d422b98
Lee Blake <charlotte.blake@bsu.edu>
2023-09-01 16:31:10 -0400

    Implement basic conversions

---
+++ src/main/java/edu/bsu/cs495/quaternary_calc/QuaternaryNumber.java
@@ -0,0 +1,59 @@
+package edu.bsu.cs495.quaternary_calc;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class QuaternaryNumber {
+
+    private final int decimalValue;
+
+    public QuaternaryNumber(String quaternaryString) {
+        verifyStringIsAQuaternaryNumber(quaternaryString);
+        this.decimalValue = parseQuaternaryNumberFromString(quaternaryString);
+    }
+
+    private void verifyStringIsAQuaternaryNumber(String possibleNumber) {
+        Pattern pattern = Pattern.compile("^-?[0123]+$");
+        Matcher matcher = pattern.matcher(possibleNumber);
+        if(!matcher.find()) {
+            throw new IllegalArgumentException("'" + possibleNumber + "' is not a quaternary number!");
+        }
+    }
+
+    private int parseQuaternaryNumberFromString(String quaternaryString) {
+        int total = 0;
+        int multiplier = 1;
+        while(!quaternaryString.equals("-") && !quaternaryString.equals("")) {
+            int lastDigit = Integer.parseInt(quaternaryString.substring(quaternaryString.length()-1));
+            total += multiplier * lastDigit;
+            multiplier *= 4;
+            quaternaryString = quaternaryString.substring(0, quaternaryString.length()-1);
+        }
+        if(quaternaryString.equals("-")) {
+            total *= -1;
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder representation = new StringBuilder();
+        int remainingValue = Math.abs(decimalValue);
+        while(remainingValue != 0) {
+            representation.insert(0, remainingValue % 4);
+            remainingValue /= 4;
+        }
+        if(decimalValue < 0) {
+            representation.insert(0, "-");
+        }
+        return representation.toString();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if(other instanceof QuaternaryNumber otherNumber) {
+            return this.decimalValue == otherNumber.decimalValue;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/edu/bsu/cs495/quaternary_calc/TestQuaternaryNumber.java
@@ -0,0 +1,92 @@
+package edu.bsu.cs495.quaternary_calc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestQuaternaryNumber {
+
+    @Test
+    public void testConstructorThrowsExceptionOnNonQuaternaryNumber() {
+        String notQuaternary = "1042";
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> new QuaternaryNumber(notQuaternary)
+        );
+    }
+
+    @Test
+    public void testConstructorWorksFineOnQuaternaryNumber() {
+        String quaternary = "-0321";
+        new QuaternaryNumber(quaternary);
+    }
+
+
+    @Test
+    public void testConstructorToStringSimpleReturnsIdentically() {
+        String constructorString = "12";
+        QuaternaryNumber number = new QuaternaryNumber(constructorString);
+        String actual = number.toString();
+        Assertions.assertEquals(constructorString, actual);
+    }
+
+    @Test
+    public void testConstructorToStringLeadingZerosAreStripped() {
+        String leadingZeros = "001313232112";
+        QuaternaryNumber number = new QuaternaryNumber(leadingZeros);
+        String actual = number.toString();
+        String expected = "1313232112";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConstructorToStringNegativeNumberReturnsIdentically() {
+        String constructorString = "-12032102";
+        QuaternaryNumber number = new QuaternaryNumber(constructorString);
+        String actual = number.toString();
+        Assertions.assertEquals(constructorString, actual);
+    }
+
+    // This is testing that incomparable types due result in 'false' - which is intended behavior in Java.
+    @SuppressWarnings("AssertBetweenInconvertibleTypes")
+    @Test
+    public void testEqualsCompareToNonNumberFalse() {
+        String otherObject = "a";
+        QuaternaryNumber number = new QuaternaryNumber("0");
+        Assertions.assertNotEquals(number, otherObject);
+    }
+
+    @Test
+    public void testEqualsSameNumberTrue() {
+        QuaternaryNumber first = new QuaternaryNumber("321");
+        QuaternaryNumber second = new QuaternaryNumber("321");
+        Assertions.assertEquals(first, second);
+    }
+
+    @Test
+    public void testEqualsOtherNumberFalse() {
+        QuaternaryNumber first = new QuaternaryNumber("11111");
+        QuaternaryNumber second = new QuaternaryNumber("22222");
+        Assertions.assertNotEquals(first, second);
+    }
+
+    @Test
+    public void testEqualsTwoEqualNegativesTrue() {
+        QuaternaryNumber first = new QuaternaryNumber("-3210");
+        QuaternaryNumber second = new QuaternaryNumber("-03210");
+        Assertions.assertEquals(first, second);
+    }
+
+    @Test
+    public void testEqualsTwoNonEqualNegativesFalse() {
+        QuaternaryNumber first = new QuaternaryNumber("-123");
+        QuaternaryNumber second = new QuaternaryNumber("-321");
+        Assertions.assertNotEquals(first,second);
+    }
+
+    @Test
+    public void testEqualsMixedSignsFalse() {
+        QuaternaryNumber positive = new QuaternaryNumber("1");
+        QuaternaryNumber negative = new QuaternaryNumber("-1");
+        Assertions.assertNotEquals(positive, negative);
+    }
+}
