b5452921160aae6747fee2ca22b52a4d6173e5a9
Beethoven Meginnis <bgmeginnis@bsu.edu>
2023-09-01 18:30:26 -0400

    Updated CalcGUITest.java to add buttons

--- src/main/java/edu/bsu/cs495/quaternary_calc/CalcGUITest.java
+++ src/main/java/edu/bsu/cs495/quaternary_calc/CalcGUITest.java
@@ -2,20 +2,10 @@ package edu.bsu.cs495.quaternary_calc;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CalcGUITest {
-    private JTextArea textArea1;
-    private JButton zeroButton;
-    private JButton oneButton;
-    private JButton twoButton;
-    private JButton threeButton;
-    private JButton additionButton;
-    private JButton subtractionButton;
-    private JButton multiplicationButton;
-    private JButton divisionButton;
-    private JButton squarerootButton;
-    private JButton squareButton;
-    private JButton enterButton;
     private double operand1;
     private double operand2;
     private String operator;
@@ -25,6 +15,37 @@ public class CalcGUITest {
         //initializing window and buttons
         JFrame frame = new JFrame("Quaternary Calculator");
         TextArea textArea1 = new TextArea();
+
+        //initialize number buttons
+        JButton zeroButton = new JButton("0");
+        JButton oneButton = new JButton("1");
+        JButton twoButton = new JButton("2");
+        JButton threeButton = new JButton("3");
+
+        //initialize operator buttons
+        JButton addButton = new JButton("+");
+        JButton subButton = new JButton("-");
+        JButton mulButton = new JButton("*");
+        JButton divButton = new JButton("/");
+        JButton sqButton = new JButton("sq");
+        JButton sqrtButton = new JButton("sqrt");
+
+
+        //intialize button panel
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(4, 3));
+
+        //add buttons to panel
+        buttonPanel.add(zeroButton);
+        buttonPanel.add(oneButton);
+        buttonPanel.add(twoButton);
+        buttonPanel.add(threeButton);
+        buttonPanel.add(addButton);
+        buttonPanel.add(subButton);
+        buttonPanel.add(mulButton);
+        buttonPanel.add(divButton);
+        buttonPanel.add(sqButton);
+        buttonPanel.add(sqrtButton);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
