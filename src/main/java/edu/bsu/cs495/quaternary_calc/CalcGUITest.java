8d1546aab0281827d7b6ee23269846a01e29ea7a
Noah M <nmmay@bsu.edu>
2023-09-01 17:53:37 -0400

    Create frame with text area for GUI

--- src/main/java/edu/bsu/cs495/quaternary_calc/CalcGUITest.java
+++ src/main/java/edu/bsu/cs495/quaternary_calc/CalcGUITest.java
@@ -1,20 +1,35 @@
 package edu.bsu.cs495.quaternary_calc;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CalcGUITest {
     private JTextArea textArea1;
-    private JButton divisionButton;
-    private JButton additionButton;
-    private JButton twoButton;
-    private JButton oneButton;
-    private JButton multiplicationButton;
-    private JButton enterButton;
     private JButton zeroButton;
-    private JButton squarerootButton;
-    private JButton subtractionButton;
-
+    private JButton oneButton;
+    private JButton twoButton;
     private JButton threeButton;
+    private JButton additionButton;
+    private JButton subtractionButton;
+    private JButton multiplicationButton;
+    private JButton divisionButton;
+    private JButton squarerootButton;
     private JButton squareButton;
+    private JButton enterButton;
+    private double operand1;
+    private double operand2;
+    private String operator;
 
+    public CalcGUITest(){
+
+        //initializing window and buttons
+        JFrame frame = new JFrame("Quaternary Calculator");
+        TextArea textArea1 = new TextArea();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.add(textArea1, BorderLayout.NORTH);
+        frame.setSize(600, 400);
+        frame.setVisible(true);
+    }
 }
