5e7287c3b4a94be27e65714c89a267fac2999064
Beethoven Meginnis <bgmeginnis@bsu.edu>
2023-09-01 16:02:04 -0400

    Create CalcGUITest.java

---
+++ src/main/java/edu/bsu/cs495/quaternary_calc/CalcGUITest.java
@@ -0,0 +1,20 @@
+package edu.bsu.cs495.quaternary_calc;
+
+import javax.swing.*;
+
+public class CalcGUITest {
+    private JTextArea textArea1;
+    private JButton divisionButton;
+    private JButton additionButton;
+    private JButton twoButton;
+    private JButton oneButton;
+    private JButton multiplicationButton;
+    private JButton enterButton;
+    private JButton zeroButton;
+    private JButton squarerootButton;
+    private JButton subtractionButton;
+
+    private JButton threeButton;
+    private JButton squareButton;
+
+}
