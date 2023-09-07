a96910256927992803e0cafe07ece20d07a629a8
Lee Blake <charlotte.blake@bsu.edu>
2023-09-07 08:46:12 -0400

    Implement evaluateIgnoringLastOperation

--- src/main/java/edu/bsu/cs495/quaternary_calc/QuaternaryCalculator.java
+++ src/main/java/edu/bsu/cs495/quaternary_calc/QuaternaryCalculator.java
@@ -5,6 +5,8 @@ package edu.bsu.cs495.quaternary_calc;
 //import java.awt.event.ActionEvent;
 
 
+import java.util.ArrayList;
+
 public class QuaternaryCalculator {
 
     public void getString() {
@@ -18,13 +20,46 @@ public class QuaternaryCalculator {
     // below is placeholder
     String expressionSymbol = "";
 
+    private final ArrayList<QuaternaryNumber> operands = new ArrayList<>();
+    private final ArrayList<BinaryOperator> operators = new ArrayList<>();
 
-    public void submitBinaryOperation() {
-        // placeholder
+    public void submitBinaryOperation(QuaternaryNumber leftOperand, BinaryOperator operator) {
+        operands.add(leftOperand);
+        operators.add(operator);
     }
 
     public QuaternaryNumber evaluateIgnoringLastOperation() {
-        return new QuaternaryNumber("0");
+        if (operands.isEmpty()) {
+            return new QuaternaryNumber("0");
+        }
+        QuaternaryNumber runningTotal = operands.get(0);
+        for (int i = 0; i < operands.size() - 1; i++) {
+            QuaternaryNumber rightOperand = operands.get(i + 1);
+            BinaryOperator operator = operators.get(i);
+            runningTotal = performOperation(runningTotal, operator, rightOperand);
+        }
+        return runningTotal;
+    }
+
+    private QuaternaryNumber performOperation(QuaternaryNumber leftOperand, BinaryOperator operator,
+                                              QuaternaryNumber rightOperand) {
+        switch (operator) {
+            case ADDITION -> {
+                return leftOperand.plus(rightOperand);
+            }
+            case SUBTRACTION -> {
+                return leftOperand.minus(rightOperand);
+            }
+            case MULTIPLICATION -> {
+                return leftOperand.times(rightOperand);
+            }
+            case DIVISION -> {
+                return leftOperand.dividedBy(rightOperand);
+            }
+            default -> {
+                throw new IllegalArgumentException("The operator has not be implemented!");
+            }
+        }
     }
 
     /*
--- src/test/java/edu/bsu/cs495/quaternary_calc/TestQuaternaryCalculator.java
+++ src/test/java/edu/bsu/cs495/quaternary_calc/TestQuaternaryCalculator.java
@@ -12,4 +12,92 @@ public class TestQuaternaryCalculator {
         QuaternaryNumber zero = new QuaternaryNumber("0");
         Assertions.assertEquals(zero, actual);
     }
+
+    @Test
+    public void testEvaluateIgnoringLastOperationOneSubmissionReturnsFirstNumber() {
+        QuaternaryCalculator calculator = new QuaternaryCalculator();
+        calculator.submitBinaryOperation(
+                new QuaternaryNumber("13"),
+                BinaryOperator.ADDITION
+        );
+        QuaternaryNumber actual = calculator.evaluateIgnoringLastOperation();
+        QuaternaryNumber expected = new QuaternaryNumber("13");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEvaluateIgnoringLastOperationIgnoresLastOperation() {
+        QuaternaryCalculator calculator1 = new QuaternaryCalculator();
+        calculator1.submitBinaryOperation(
+                new QuaternaryNumber("13"),
+                BinaryOperator.ADDITION
+        );
+        calculator1.submitBinaryOperation(
+                new QuaternaryNumber("21"),
+                BinaryOperator.SUBTRACTION
+        );
+        QuaternaryNumber result1 = calculator1.evaluateIgnoringLastOperation();
+        QuaternaryCalculator calculator2 = new QuaternaryCalculator();
+        calculator2.submitBinaryOperation(
+                new QuaternaryNumber("13"),
+                BinaryOperator.ADDITION
+        );
+        calculator2.submitBinaryOperation(
+                new QuaternaryNumber("21"),
+                BinaryOperator.ADDITION
+        );
+        QuaternaryNumber result2 = calculator2.evaluateIgnoringLastOperation();
+        Assertions.assertEquals(result1, result2);
+    }
+
+    @Test
+    public void testEvaluateIgnoringLastOperationMultipleAdditionSubmissionsReturnSum() {
+        QuaternaryCalculator calculator = new QuaternaryCalculator();
+        calculator.submitBinaryOperation(
+                new QuaternaryNumber("13"),
+                BinaryOperator.ADDITION
+        );
+        calculator.submitBinaryOperation(
+                new QuaternaryNumber("3"),
+                BinaryOperator.ADDITION
+        );
+        calculator.submitBinaryOperation(
+                new QuaternaryNumber("2"),
+                BinaryOperator.ADDITION
+        );
+        QuaternaryNumber actual = calculator.evaluateIgnoringLastOperation();
+        QuaternaryNumber expected = new QuaternaryNumber("30");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEvaluateIgnoringLastOperationAllOperationsSequencePerformsCorrectGreedyCalculation() {
+        // In decimal, this will be 3+6-2*5/2 and should return 17 if using the classical calculator
+        // approach of "greedy" application of operations and this assignment's integer division
+        QuaternaryCalculator calculator = new QuaternaryCalculator();
+        calculator.submitBinaryOperation(
+                new QuaternaryNumber("3"),
+                BinaryOperator.ADDITION
+        );
+        calculator.submitBinaryOperation(
+                new QuaternaryNumber("12"),
+                BinaryOperator.SUBTRACTION
+        );
+        calculator.submitBinaryOperation(
+                new QuaternaryNumber("2"),
+                BinaryOperator.MULTIPLICATION
+        );
+        calculator.submitBinaryOperation(
+                new QuaternaryNumber("11"),
+                BinaryOperator.DIVISION
+        );
+        calculator.submitBinaryOperation(
+                new QuaternaryNumber("2"),
+                BinaryOperator.DIVISION
+        );
+        QuaternaryNumber actual = calculator.evaluateIgnoringLastOperation();
+        QuaternaryNumber expected = new QuaternaryNumber("101");
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
